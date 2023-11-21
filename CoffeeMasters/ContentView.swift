11acb44607eab0416063e82dbcd4f266aaa04151
davidliberal0 <davidliberal0@gmail.com>
2023-11-20 22:02:44 -0500

    add navigation tabs

--- CoffeeMasters/ContentView.swift
+++ CoffeeMasters/ContentView.swift
@@ -11,7 +11,28 @@ struct ContentView: View {
     @State var name: String = ""
     
     var body: some View {
-        OffersPage()
+        TabView {
+            Text("Home Page")
+                .tabItem {
+                    Image(systemName: "cup.and.saucer")
+                    Text("Home Page")
+                }
+            OffersPage()
+                .tabItem {
+                    Image(systemName: "tag")
+                    Text("Offers")
+                }
+            Text("Oder Page")
+                .tabItem {
+                    Image(systemName: "cart")
+                    Text("Order")
+                }
+            Text("Info Page")
+                .tabItem {
+                    Image(systemName: "info")
+                    Text("Info")
+                }
+        }
     }
 }
 
