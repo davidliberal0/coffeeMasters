4e279b2df6a93b9474257bd73a93701d7f3f7d29
davidliberal0 <davidliberal0@gmail.com>
2023-11-21 16:00:27 -0500

    fix detail page format and logic

--- CoffeeMasters/PageViews/DetailsPage.swift
+++ CoffeeMasters/PageViews/DetailsPage.swift
@@ -11,9 +11,12 @@ struct DetailsPage: View {
     // state variable for holding quantity
     @State var quantity = 1
     
+    // variable to store the product
+    var product: Product
+    
     var body: some View {
         ScrollView {
-            Image("DummyImage")
+            AsyncImage(url: product.imageURL)
                 .cornerRadius(5)
                 .frame(maxWidth: .infinity, idealHeight: 150, maxHeight: 150)
                 .padding(.top, 32)
@@ -22,7 +25,7 @@ struct DetailsPage: View {
                 .multilineTextAlignment(.leading)
                 .padding(24)
             HStack {
-                Text("$ 4.25 ea")
+                Text("$ \(product.price, specifier: "%.2f" ) each")
                 // implement stepper to change quatity
                 Stepper(value: $quantity, in: 1...10) { }
             }
@@ -33,7 +36,7 @@ struct DetailsPage: View {
                 .bold()
                 .padding(12)
             
-            Button("Add \(quantity) to Cart") {
+            Button("Add \(Double(quantity)*product.price, specifier: "%.2f") to Cart") {
                 //TODO
             }
                 .padding()
@@ -48,5 +51,5 @@ struct DetailsPage: View {
 }
 
 #Preview {
-    DetailsPage()
+    DetailsPage(product: Product(id: 1, name: "Dummy", description: "", price: 1.25, image: ""))
 }
